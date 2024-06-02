8c9fa36d0c6f64d165eebc6832e238abdb884e85
MitchellJustinMSantamena <138697740+MitchellJustinMSantamena@users.noreply.github.com>
2024-06-02 22:03:31 +0800

    Create module 2.sol

---
+++ module 2.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
+    }
+
+    function getBalance() public view returns(uint256) {
+        return balance;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
+
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+        
+        // ensure the amount is between 1 and 10
+        require(_amount >= 1 && _amount <= 10, "Amount must be between 1 and 10");
+
+        // perform transaction
+        balance += _amount;
+
+        // assert transaction completed successfully
+        assert(balance == _previousBalance + _amount);
+
+        // emit the event
+        emit Deposit(_amount);
+    }
+
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        
+        require(_withdrawAmount >= 1 && _withdrawAmount <= 10, "Amount must be between 1 and 10");
+
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        balance -= _withdrawAmount;
+
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        emit Withdraw(_withdrawAmount);
+    }
+}
